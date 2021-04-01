0a8018fbfcfbeed88cdfcfc02c6c99844cd21a52
SikirskyiOleh <sikirskyi.oleh@icloud.com>
2021-04-01 18:56:12 +0300

    add practice 1 (1.1-1.14) and homework 2 (2.1-2.20)

---
+++ stage-js/homework/2/2.3-2.6/script.js
@@ -0,0 +1,31 @@
+//  -3 За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+
+// for (let i = 0; i < 10; i++) {
+//     document.write("<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio pariatur quo sapiente temporibus vitae voluptas. Alias aperiam autem incidunt laborum minus quia reiciendis sed soluta velit voluptates. Accusantium, dicta, nulla?</div>");
+// }
+//________________________________________________________________________________
+
+// -4 За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+
+// for (let i = 0; i < 10; i++) {
+// let index = i;
+//     document.write(`<div> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio pariatur quo sapiente temporibus vitae voluptas. Alias aperiam autem incidunt laborum minus quia reiciendis sed soluta velit voluptates. Accusantium, dicta, nulla? ${index}</div>`);
+// }
+//__________________________________________________________________________________
+
+// -5 За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+
+// let i = 0;
+// while (i < 20) {
+//     document.write("<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque eligendi impedit nam perspiciatis quae sit sunt temporibus voluptates! Ad at aut blanditiis consequatur corporis delectus dolor doloremque et ex fugiat?</h1>")
+//     i++;
+// }
+//_____________________________________________________________________________________
+
+// -6 За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині
+
+// let i = 0;
+// while (i < 20) {
+//     document.write(`<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque eligendi impedit nam perspiciatis quae sit sunt temporibus voluptates! Ad at aut blanditiis consequatur corporis delectus dolor doloremque et ex fugiat? ${i}</h1>`)
+//     i++;
+// }
