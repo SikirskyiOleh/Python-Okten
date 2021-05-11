ed8a3fce158b2f43b91757fe2aeb9f83af2ccb5b
SikirskyiOleh <sikirskyi.oleh@icloud.com>
2021-05-11 15:20:29 +0300

    create stage-react

---
+++ stage-js/homework/7/script.js
@@ -0,0 +1,36 @@
+// 1. Створити довільний елемент с id = text. використовуючи JavaScript, зробіть так, щоб при кліку на кнопку зникавл елемент с id="text".
+
+// let one = document.createElement("div");
+// one.innerText = "Paragraph";
+// document.body.appendChild(one);
+//
+// let element = document.querySelector("div");
+// element.setAttribute('id', 'text');
+//
+// let delet = document.getElementById('form');
+// delet.onclick = (ev) => {
+//     ev.preventDefault();
+//     element.remove();
+// };
+
+// 2. Створіть кнопку, при кліку на яку, вона буде приховувати сама себе.
+
+// let delet = document.getElementById('deleteMyself');
+// delet.onclick = (ev) => {
+//     ev.preventDefault();
+//     delet.remove();
+// };
+
+// 3. Створити інпут який приймає вік людини та кнопку яка підтверджує дію.
+//     При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік чи менше він ніж 18, та повідомити про це користувача
+
+// let form = document.forms.form;
+// form.Ok.onclick = (ev) => {
+//     ev.preventDefault();
+//     let val = form.age.value;
+//     val >= 18 ? alert("Age => 18") : alert("Age < 18");
+// }
+
+// 4.  Створіть меню, яке розгортається/згортається при клику
+
+
