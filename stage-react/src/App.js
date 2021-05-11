06989592402c6c0b010ca87ea9cb655cc2b3bf90
SikirskyiOleh <sikirskyi.oleh@icloud.com>
2021-05-11 15:43:35 +0300

    cl

--- stage-react/src/App.js
+++ stage-react/src/App.js
@@ -1,12 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+export default function App() {
+    return (
+        <div>
 
-function App() {
-  return (
-    <div className="App">
-     ku
-    </div>
-  );
-}
-
-export default App;
+        </div>
+    );
+};
