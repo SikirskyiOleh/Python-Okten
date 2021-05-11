95582bbbc01d462cc692a02cd3b2d423f5e7b9d9
SikirskyiOleh <sikirskyi.oleh@icloud.com>
2021-05-11 15:41:16 +0300

    add branch with homework 1

--- stage-react/src/App.js
+++ stage-react/src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Car from "./components/Car";
 
-function App() {
-  return (
-    <div className="App">
-     ku
-    </div>
-  );
+export default function App() {
+    return (
+        <div>
+            <Car
+                model={'bmw'}
+                description={'335'}
+                power={332}
+                volume={3.5}/>
+            <Car
+                model={'toyota'}
+                description={'supra'}
+                power={2000}
+                volume={3}/>
+            <Car
+                model={'mers'}
+                description={'g63'}
+                power={500}
+                volume={5}/>
+        </div>
+    );
 }
-
-export default App;
