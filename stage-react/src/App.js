61ea2195de8791e3e2e85336acb04ac6b5e0166c
SikirskyiOleh <sikirskyi.oleh@icloud.com>
2021-05-24 00:29:48 +0300

    modify master

--- stage-react/src/App.js
+++ stage-react/src/App.js
@@ -1,51 +1,7 @@
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
-import Users from './components/users/Users'
-import Home from "./components/home/Home";
-import Posts from "./components/posts/Posts";
-import Comments from "./components/comments/Comments";
-
-function App() {
+export default function App() {
     return (
-        <Router>
-            <div>
-                <Link to={'/'}>home</Link>
-                <br/>
-                <Link to={'/users'}>users </Link>
-                <br/>
-                <Link to={'/posts'}>posts</Link>
-                <br/>
-                <Link to={'/comments'}>comments</Link>
-                <br/>
-                <hr/>
+        <div>
 
-                <Switch>
-
-                    <Route exact path={'/'}>
-                        <Home/>
-                    </Route>
-
-                    <Route path={'/users'} component={Users}/>
-                    {/*add exact up
-                    <Route path={'/users/:id'} component={UserDetails}/>*/
-                    }
-
-                    <Route path={'/posts'} component={Posts}/>
-
-                    {/*<Route path={'/posts'}*/}
-                    {/*       render={() => (<Posts/>)}/>*/}
-
-                    <Route path={'/comments'} component={Comments}/>
-                    <hr/>
-                </Switch>
-
-            </div>
-        </Router>
+        </div>
     );
 };
-
-export default App;
