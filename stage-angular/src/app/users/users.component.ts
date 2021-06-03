c8fc37bdbbdf499f2b2e203a96271ddda3e0a842
SikirskyiOleh <sikirskyi.oleh@icloud.com>
2021-06-03 17:11:15 +0300

    add branch ls1

---
+++ stage-angular/src/app/users/users.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {UserService} from "../services/user/user.service";
+import {User} from "../models/user/user";
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+  users: User[];
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe(value => this.users = value);
+  }
+
+}
